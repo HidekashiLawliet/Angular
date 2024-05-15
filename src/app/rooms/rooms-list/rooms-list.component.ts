842663483f44eaefe3cba87dd70d719765ebbd3d
Hidekashi <hideaki.lawliet@proton.me>
2024-05-15 10:36:24 +0200

    fix {table error } + ADD {Add bouton room selector}

--- src/app/rooms/rooms-list/rooms-list.component.ts
+++ src/app/rooms/rooms-list/rooms-list.component.ts
@@ -1,18 +1,23 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Room, roomsList } from '../rooms';
 
 @Component({
-  selector: 'hinv-rooms-list',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './rooms-list.component.html',
-  styleUrl: './rooms-list.component.css'
+	selector: 'hinv-rooms-list',
+	standalone: true,
+	imports: [CommonModule, RouterOutlet],
+	templateUrl: './rooms-list.component.html',
+	styleUrl: './rooms-list.component.css'
 })
 export class RoomsListComponent implements OnInit {
-  @Input() rooms: roomsList[] = [];
+	@Input() rooms: roomsList[] = [];
+	@Output() selectedRoom = new EventEmitter<roomsList>();
 
-  ngOnInit(): void {}
+	ngOnInit(): void { }
+
+	selectRoom(room: roomsList) {
+		this.selectedRoom.emit(room);
+	}
 
 }
--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -5,16 +5,32 @@ import { Room, roomsList } from './rooms';
 import { RoomsListComponent } from "./rooms-list/rooms-list.component";
 
 @Component({
-    selector: 'hinv-rooms',
-    standalone: true,
-    templateUrl: './rooms.component.html',
-    styleUrl: './rooms.component.css',
-    imports: [CommonModule, RouterOutlet, RoomsListComponent]
+	selector: 'hinv-rooms',
+	standalone: true,
+	templateUrl: './rooms.component.html',
+	styleUrl: './rooms.component.css',
+	imports: [CommonModule, RouterOutlet, RoomsListComponent]
 })
 
 
 export class RoomsComponent implements OnInit {
-	ngOnInit(): void {}
+
+	selectedRoom!: roomsList
+
+	selectRoom(room: roomsList) {
+		// console.log(this.selectedRoom.booked += 1);
+		console.log(room.booked);
+		// if (room.booked == 0) {
+		// 	window.alert('There is no more room available with this option');
+		// } else {
+		// 	room.booked -= 1;
+		// 	console.log(room);
+		// }
+	}
+	ngOnInit(): void {
+
+	}
+
 	hotelName = 'Hide Hotel';
 	hideAvailableRooms = true;
 	randomNumber = 17871263987;
@@ -24,10 +40,10 @@ export class RoomsComponent implements OnInit {
 			booked: 2,
 			numberOfRooms: 3,
 			roomName: 'Voyager One room',
-			amenities:'Air conditionner, Free Wi-fi, parking spot, TV, Bathroom, spa',
+			amenities: 'Air conditionner, Free Wi-fi, parking spot, TV, Bathroom, spa',
 			price: 10000,
 			themes: 'Space',
-			photos:'https://i.pinimg.com/originals/b2/ab/ea/b2abea19989a0694c54c8899023cf3a9.jpg',
+			photos: 'https://i.pinimg.com/originals/b2/ab/ea/b2abea19989a0694c54c8899023cf3a9.jpg',
 			checkInTime: new Date('11-November-2021'),
 			checkOutTime: new Date('11-december-2022'),
 			rating: 4.826326324,
@@ -36,7 +52,7 @@ export class RoomsComponent implements OnInit {
 			booked: 2,
 			numberOfRooms: 3,
 			roomName: 'Forest room',
-			amenities:'Air conditionner, parking spot, Bathroom, spa, swimming pool',
+			amenities: 'Air conditionner, parking spot, Bathroom, spa, swimming pool',
 			price: 10200,
 			themes: 'Forest',
 			photos: 'https://www.hotels2see.com/sites/default/files/images/rustic-style-luxury-accommodation-hotel-nothofagus-chile.jpg',
@@ -48,17 +64,15 @@ export class RoomsComponent implements OnInit {
 			booked: 2,
 			numberOfRooms: 3,
 			roomName: 'Underground room',
-			amenities:'Air conditionner, Free Wi-fi (optical fiber), TV, parking spot, TV, Bathroom, jet tub, gym, gym room',
+			amenities: 'Air conditionner, Free Wi-fi (optical fiber), TV, parking spot, TV, Bathroom, jet tub, gym, gym room',
 			price: 10500,
 			themes: 'Underground',
-			photos:'https://i.ytimg.com/vi/uwLQhnyc3Mc/maxresdefault.jpg',
+			photos: 'https://i.ytimg.com/vi/uwLQhnyc3Mc/maxresdefault.jpg',
 			checkInTime: new Date('11-November-2021'),
 			checkOutTime: new Date('11-december-2022'),
 			rating: 4.52312351325,
 		},
 	];
-
-	
 
 	roomsDisponibility: Room = {
 		totalRooms: 9,
@@ -67,6 +81,6 @@ export class RoomsComponent implements OnInit {
 	};
 
 	toggle() {
-    this.hideAvailableRooms = !this.hideAvailableRooms;
+		this.hideAvailableRooms = !this.hideAvailableRooms;
 	}
 }
