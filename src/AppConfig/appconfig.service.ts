1bba38f41fed6220acea51b60badd786a1cd02e8
Hidekashi <hideaki.lawliet@proton.me>
2024-05-21 15:41:35 +0200

    FIX{setting files update}

--- src/AppConfig/appconfig.service.ts
+++ src/AppConfig/appconfig.service.ts
@@ -5,11 +5,9 @@ export const APP_SERVICE_CONFIG = new InjectionToken<AppConfig>('AppConfig');
 export interface AppConfig {
     apiEndpoint: string;
     title: string;
-    baseUrl: string;
 }
 
 export const APP_CONFIG: AppConfig = {
-    apiEndpoint: 'http://localhost:3000',
+    apiEndpoint: 'http://localhost:4200',
     title: 'Hotel Inventory App',
-    baseUrl: 'http://localhost:4200'
 };
